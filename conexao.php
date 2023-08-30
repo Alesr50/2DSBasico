562d469c8626f5a478be07c0069cdfa72907de2f
Alesr50 <ale.sr50@gmail.com>
2023-08-29 22:17:41 -0300

    update conexao

--- conexao.php
+++ conexao.php
@@ -1,18 +1,13 @@
 
 <?php
 function conectar(){
-
-
  # Informações sobre o banco de dados:
-	
-
     //Servidor, Nome do Banco, Usuario e Senha, Driver do MYSQl
     $db_host = "localhost";
     $db_nome = "dbloja";
     $db_usuario = "root";
     $db_senha = "";
     $db_driver = "mysql";
-
 
 try{
     // Faz conexão com banco de daddos
